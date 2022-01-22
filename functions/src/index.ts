1614ff65f7be2075a84ecaa998e1ff00b6a7cb3d
Shkaffez <shishkov-i@mail.com>
2022-01-22 15:16:14 +0300

    add auth routes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,23 +2,38 @@ import * as functions from "firebase-functions";
 import * as express from "express";
 import "dotenv/config";
 import * as admin from "firebase-admin";
-import * as cors from "cors";
+import * as csrf from "csurf";
+import * as bodyParser from "body-parser";
+import * as cookieParser from "cookie-parser";
 
 admin.initializeApp();
+
+const csrfMiddleware = csrf({cookie: true});
 
 const db = admin.database();
 
 const app = express();
-app.use(express.json());
-app.use(cors({origin: true}));
+app.use(bodyParser());
+app.use(cookieParser());
+app.use(csrfMiddleware);
+
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+app.all("*", (req, res, next) => {
+  res.cookie("XSRF-TOKEN", req.csrfToken());
+  next();
+});
 
 
-app.get("/", async (req, res) => {
+// crud for book block
+
+app.get("/api/books", async (req, res) => {
   const books = (await db.ref("books").once("value")).val();
   res.json(books || {});
 });
 
-app.post("/", async (req, res) => {
+app.post("/api/books", async (req, res) => {
   const book = req.body;
   const bookId = await db.ref("books").push(book);
   const bookObj = await db.ref("books").child(bookId.key!).once("value");
@@ -28,7 +43,7 @@ app.post("/", async (req, res) => {
   });
 });
 
-app.get("/:id", async (req, res) => {
+app.get("/api/books/:id", async (req, res) => {
   const {id} = req.params;
   const book = await db.ref("books").child(id).once("value");
   if (!book.val()) {
@@ -38,13 +53,13 @@ app.get("/:id", async (req, res) => {
 });
 
 
-app.delete("/:id", async (req, res) => {
+app.delete("/api/books/:id", async (req, res) => {
   const {id} = req.params;
   await db.ref("books").child(id).remove();
   res.json("ok");
 });
 
-app.put("/:id", async (req, res) => {
+app.put("/api/books/:id", async (req, res) => {
   const {id} = req.params;
   const book = req.body;
   const bookObj = await db.ref("books").child(id).once("value");
@@ -61,8 +76,104 @@ app.put("/:id", async (req, res) => {
   res.json(updatedBook);
 });
 
+// auth block
 
-exports.crud = functions.https.onRequest(app);
+app.get("/login", function(req, res) {
+  res.render("login.ejs", {title: "Авторизация"});
+});
+
+app.get("/signup", function(req, res) {
+  res.render("signup.ejs", {
+    title: "Регистрация",
+    csrfToken: req.csrfToken(),
+  });
+});
+
+app.post("/signup", function(req, res) {
+  const {name, email, password} = req.body;
+  console.log("req.body", req.body);
+  admin.auth().createUser({
+    email: email,
+    password: password,
+    displayName: name,
+  })
+      .then((userRecord) => {
+        console.log("Successfully created new user:", userRecord.uid);
+        res.redirect("/login");
+      })
+      .catch((error) => {
+        console.log("Error creating new user:", error);
+        res.redirect("/signup");
+      });
+});
+
+
+app.post("/sessionLogin", (req, res) => {
+  const idToken = req.body.idToken.toString();
+
+  const expiresIn = 60 * 60 * 24 * 5 * 1000;
+
+  admin
+      .auth()
+      .createSessionCookie(idToken, {expiresIn})
+      .then(
+          (sessionCookie) => {
+            res.cookie("session", sessionCookie);
+            res.json({status: "success"});
+          },
+          (error) => {
+            console.error("error", error);
+            res.status(401).redirect("/login");
+          }
+      );
+});
+
+app.get("/profile", function(req, res) {
+  const sessionCookie = req.cookies.session || "";
+
+  admin
+      .auth()
+      .verifySessionCookie(sessionCookie)
+      .then((userRecord) => {
+        res.render("profile.ejs", {
+          title: "Профиль",
+          userRecord: userRecord,
+        });
+      })
+      .catch((error) => {
+        res.redirect("/login");
+      });
+});
+
+app.get("/", function(req, res) {
+  res.render("index.ejs", {title: "Главная"});
+});
+
+app.post("/sessionLogin", (req, res) => {
+  const idToken = req.body.idToken.toString();
+  const expiresIn = 60 * 60 * 24 * 5 * 1000;
+
+  admin
+      .auth()
+      .createSessionCookie(idToken, {expiresIn})
+      .then(
+          (sessionCookie: any) => {
+            res.cookie("session", sessionCookie);
+            res.json({status: "success"});
+          },
+          (error: any) => {
+            res.status(401).send("UNAUTHORIZED REQUEST!");
+          }
+      );
+});
+
+app.get("/sessionLogout", (req, res) => {
+  res.clearCookie("session");
+  res.redirect("/login");
+});
+
+
+exports.app = functions.https.onRequest(app);
 
 exports.moderator = functions.database.ref("/books/{bookId}").onWrite((change) => {
   const book = change.after.val();
